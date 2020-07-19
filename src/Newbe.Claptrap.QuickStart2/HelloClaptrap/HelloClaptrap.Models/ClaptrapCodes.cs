90521f1770732b0868a68b97ae8f7d4eac8fe160
Newbe36524 <newbe36524@qq.com>
2020-07-19 11:43:22 +0800

    Feature/start 2 (#10)
    * add project for quick start step 2
    
    * update README

---
+++ src/Newbe.Claptrap.QuickStart2/HelloClaptrap/HelloClaptrap.Models/ClaptrapCodes.cs
@@ -0,0 +1,11 @@
+namespace HelloClaptrap.Models
+{
+    public static class ClaptrapCodes
+    {
+        public const string CartGrain = "cart_claptrap_newbe";
+        private const string CartEventSuffix = "_e_" + CartGrain;
+        public const string AddItemToCart = "addItem" + CartEventSuffix;
+        public const string RemoveItemFromCart = "removeItem" + CartEventSuffix;
+        public const string RemoveAllItemsFromCart = "remoeAllItems" + CartEventSuffix;
+    }
+}
